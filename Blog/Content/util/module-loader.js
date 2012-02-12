f7c6cbddd1b388b8055f0e0b7179094e02e7c968
kharlamov <kharlamov@gmail.com>
2012-02-12 14:16:42 -0700

    Updated module loader to wait for require and hide page until all content is loaded.

--- Blog/Content/util/module-loader.js
+++ Blog/Content/util/module-loader.js
@@ -1,16 +1,21 @@
-﻿define(['util/jquery', 'util/js-loader'], function ($, jL) {
-  var loader = {}, attr = 'data-module', module;
+﻿define(['util/jquery', 'util/js-loader', 'util/domReady'], function ($, jL, domReady) {
+  var loader = {},
+      page = $('html'),
+      attr = 'data-module',
+      module;
 
-  loader.load = function (){
-    $('['+attr+']').each(function() {
+  loader.load = function () {
+    $('[' + attr + ']').each(function () {
       module = $(this);
       $.ajax({
         url: module.attr(attr),
-        success: function(moduleHtml) {
+        async: false,
+        success: function (moduleHtml) {
           module.html(jL.load(moduleHtml));
         }
       });
     });
+    domReady(function () { page.show(); });
   };
 
   return loader;
